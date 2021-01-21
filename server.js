2557ba8e0654fb80c293fb24b267221191034d0d
Ocampo <mariaocampo0513@gmail.com>
2021-01-21 15:10:38 -0800

    set up of server.js file

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const app = require ("express");
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const exphbs = require ("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static("public"));
+// urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// json
+app.use(bodyParser.json());
+
+// templating engine. defaultlayout main
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//routes
+const routes= require("./controllers/burger_controller")
+app.use(routes);
+
+app.listen(PORT,()=>console.log("server listening on `${PORT}`"));
