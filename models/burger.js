77afaf442ee4ac90d743b870b0553691c4172876
Ocampo <mariaocampo0513@gmail.com>
2021-01-23 18:54:10 -0800

    completed the burger model. Created the code that will call the ORM fucntions using burger specific input for the ORM

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,33 @@
 const orm = require("../config/orm")
   
-  // create the code that will call the ORM functions using burger specific input for the ORM.
+// create the code that will call the ORM functions using burger specific input for the ORM.
 
-    // * Export at the end of the `burger.js` file.
+// * Export at the end of the `burger.js` file.
 
-    const burger = {
-        all: function(cb){
-            orm.all("burgers", res=>{
-                cd(res);
-            })
-        }
-}, 
+const burger = {
+    all: function(cb){
+        orm.all("burgers", res=>{
+            cd(res);
+        });
+    },
+
+    create: function (cols, vals, cb){
+        orm.create("burgers", cols, vals, (res)=>{
+            cb(res);
+        })
+    }, 
+
+    update: function (objColVals, condition, cb){
+        orm.update("burgers", ObjColVals, condition, (res)=>{
+            cb(res);
+        });
+    }, 
+
+    delete:function (condition, cb){
+        orm.delete("burgers", condition, (res)=>{
+            cb(res);
+        });
+    }
+} 
+
+module.exports = burger;
