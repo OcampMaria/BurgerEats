4a667f90753e7b9c5705d88cdae09ac6a4006b9b
Ocampo <mariaocampo0513@gmail.com>
2021-01-23 19:11:12 -0800

    Added helper functions for SQL syntax inside orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,39 @@
 const connection = require("./connection")
 
+
+
+function printQMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+    return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = `'  ${value}  '`;
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(`${key} = ${value}`);
+      }
+    }
+    // translate array of strings to a single comma-separated string
+
+    return arr.toString();
+};
+
+
+
 const orm = {
    selectAll: function(table, cb){
     const queryString = `SELECT * FROM ${table};`;
@@ -18,20 +52,19 @@ const orm = {
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
+        queryString += printQMarks(vals.length);
         queryString += ") ";
 
         console.log(queryString);
 
         connection.query(queryString, (err,res)=>{
             if (err) throw err;
-    
             cb(res);
         });   
     },
 
     updateOne: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+        const queryString = "UPDATE " + table;
 
         queryString += " SET ";
         queryString += objToSql(objColVals);
@@ -39,11 +72,22 @@ const orm = {
         queryString += condition;
 
         console.log(queryString);
+
         connection.query(queryString, (err,res)=>{
             if (err) throw err;
-
             cb(res);
         });  
+    }, 
+
+    deleteOne: function(table, condition, cb){
+        const queryString = `DELETE FROM ${table} WHERE ${condition};`
+
+        console.log(queryString);
+
+        connection.query(queryString, (err, res)=>{
+            if(err) throw err;
+            cb(res);
+        })
     }
 }
 // create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
